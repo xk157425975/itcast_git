e7e421e94d130e7757e8d9ce071302beec154cb2
xk157425975 <157425975@qq.com>
2018-12-08 16:19:06 +0800

    11

--- git_day01_projeck/src/main/java/com/itheima/domain/User.java
+++ git_day01_projeck/src/main/java/com/itheima/domain/User.java
@@ -1,8 +1,11 @@
 package com.itheima.domain;
 
+import java.util.Date;
+
 public class User {
     private  int id;
     private  String name;
     private  String address;
+    private Date date;
 
 }
