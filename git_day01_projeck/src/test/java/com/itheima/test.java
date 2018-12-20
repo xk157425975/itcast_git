35d73a4c980aee02f40cdf1424dd7f9846bd7221
xk157425975 <157425975@qq.com>
2018-12-20 21:01:46 +0800

    做了一场修改

---
+++ git_day01_projeck/src/test/java/com/itheima/test.java
@@ -0,0 +1,10 @@
+package com.itheima;
+
+import com.itheima.es.domain.User;
+
+class Test {
+    public static void main(String[] args) {
+        User user =new User();
+        System.out.println(user.getId());
+    }
+}
